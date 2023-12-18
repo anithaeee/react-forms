4308b62140711175a2addbbc3e58f679fbd6ae8d
anithaeee <anithaeee224@gmail.com>
2023-12-18 11:28:31 +0800

    dec18th

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+  const[name,setname]= useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <form>
+       <label>enter your name:
+        <input type="text" value={name} onC={(e)=>{setname(e.target.value)}}hange></input>
+       </label>
+     </form>
+    </>
   );
 }
 
