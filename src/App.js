7ecaf3ffecf4900029a01300c5244dc56dd18250
anithaeee <anithaeee224@gmail.com>
2023-12-19 11:53:48 +0800

    dec19th

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React,{useState} from 'react';
 function App() {
+  const[value,setvalue] = useState("");
   const[name,setname]= useState('');
+
+   const handleSubmit = (val)=>{
+      setvalue(val);
+      alert("form got submitted" +value)
+   }
   return (
     <>
-     <form>
+     <form  onSubmit={()=>{handleSubmit(name)}}> 
        <label>enter your name:
-        <input type="text" value={name} onChange={(e)=>{setname(e.target.value)}}hange></input>
+        <textarea type="text" value={name} onChange={(e)=>{setname(e.target.value)}}hange></textarea>
        </label>
+       <input type='submit' value="Submit"></input>
      </form>
     </>
   );
